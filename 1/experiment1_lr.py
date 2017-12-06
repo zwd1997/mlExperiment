f2d82b4f599ceaf255f55ac3dc09ed148110dbf7
sewindyzhu <hrxiloveyou@gmail.com>
2017-12-06 15:49:56 +0800

    train and validation data comparision complited

--- 1/experiment1_lr.py
+++ 1/experiment1_lr.py
@@ -13,27 +13,38 @@ def compute_error(b,m,x,y):
     N = len(y)
     #m.shape=(13,1)
     x.shape=(N,13)
-    temp=np.ones((N,1))*b
-    totalError=(y-x.todense().dot(m)-temp)
+    #temp=np.ones((N,1))*b
+    totalError=(y-x.todense().dot(m)-b)
     totalError=totalError.T.dot(totalError)
-    totalError=np.sum(totalError,axis=0)
+    #totalError=np.sum(totalError,axis=0)
+    totalError=np.sum(totalError)
 
     return totalError/float(len(y))
 
 
-def optimizer(x,y,starting_b,starting_m,learning_rate,num_iter):
+def optimizer(x,y,x_test,y_test,starting_b,starting_m,learning_rate,num_iter):
     b=starting_b
     m=starting_m
-    error_list = np.arange(num_iter)
+    error_list1 = np.arange(num_iter)
+    error_list2 = np.arange(num_iter)
+    br, mr = compute_gradient(starting_b, starting_m, x_test, y_test, learning_rate)
 
     for i in range(num_iter):
-        b,m=compute_gradient(b,m,x,y,learning_rate)
-        if(i%100==0):
+        if(i==0):
+            b,m=compute_gradient(b,m,x,y,learning_rate)
+            b+=br
+            m+=mr
+        else:
+            b, m = compute_gradient(b, m, x, y, learning_rate)
+        if(i%10==0):
             temp = compute_error(b,m,x,y)
             print ('iter {0}:error={1}'.format(i,temp))
-            error_list[int(i/100)]=temp
-        error_list[i]=temp
-    return[b,m,error_list]
+            #error_list[int(i/100)]=temp
+        temp = compute_error(b, m, x, y)
+        error_list1[i] = temp
+        temp2 = compute_error(b,m,x_test,y_test)
+        error_list2[i] = temp2
+    return[b,m,error_list1,error_list2]
 
 
 def compute_gradient(b_current,m_current,x,y,learning_rate):
@@ -45,12 +56,12 @@ def compute_gradient(b_current,m_current,x,y,learning_rate):
     N = float(n)
     #x = data[:,0]
     #y = data[:,1]
-    temp = np.ones((n, 1))*b_current
+    #temp = np.ones((n, 1))*b_current
     y.shape=(n,1)
     m_current.shape=(13,1)
-    b_gradient = -(2/N)*(y-x.todense().dot(m_current)-temp)
+    b_gradient = -(2/N)*(y-x.todense().dot(m_current)-b_current)
     b_gradient = b_gradient.mean()
-    m_gradient = -(2/N)*x.todense().T*(((y-x.todense().dot(m_current)-temp)))
+    m_gradient = -(2/N)*x.todense().T*(((y-x.todense().dot(m_current)-b_current)))
     m_gradient = m_gradient.mean(axis=0)
 
     new_b = b_current-(learning_rate*b_gradient)
@@ -58,7 +69,7 @@ def compute_gradient(b_current,m_current,x,y,learning_rate):
     return[new_b,new_m]
 
 
-def plot_data(error):
+def plot_data(error,error2):
     #x = data[:,0]
     #y = data[:,1]
     #y_predict = x.todense().dot(m)+b
@@ -67,7 +78,9 @@ def plot_data(error):
     #pylab.show()
     n = range(len(error))
     #x = np.arange(n)*100
-    pylab.plot(n,error)
+    pylab.plot(n,error,label='train')
+    pylab.plot(n,error2,label='test')
+    plt.legend()
     pylab.show()
 
 
@@ -84,19 +97,20 @@ def Linear_regression():
     init_m = np.zeros(13)
     num_iter = 100000
     error = 0
+    error2 = 0
 
     #print ('initial variables:\n initial_b = {0}\n intial_m = {1}\n error of begin = {2} \n'\
         #.format(init_b,init_m,compute_error(init_b,init_m,x_train,y_train)))
 
     #optimizing b and m
-    [b ,m,error] = optimizer(x_train,y_train,init_b,init_m,learning_rate,num_iter)
+    [b ,m,error,error2] = optimizer(x_train,y_train,x_test,y_test,init_b,init_m,learning_rate,num_iter)
 
     #print final b m error
     print ('final formula parmaters:\n b = {1}\n m={2}\n error of end = {3} \n'.format(num_iter,b,m,compute_error(b,m,x_train,y_train)))
 
     #plot result
     #plot_data(x_train,y_train,b,m)
-    plot_data(error)
+    plot_data(error,error2)
 
 
 if __name__ =='__main__':
