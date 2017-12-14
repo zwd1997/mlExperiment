9726fbd9def64f4ecd74006d4ff9653c8b4176cb
sewindyzhu <hrxiloveyou@gmail.com>
2017-12-15 06:47:29 +0800

    init 2

--- 2/experiment2_lr.py
+++ 2/experiment2_lr.py
@@ -5,68 +5,71 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_svmlight_file
 import matplotlib.pyplot as plt
 import math
+import random
 
 
 def sigmod(x):
-    temp = 1/(1+math.exp(-x))
+    temp = 1/(1+np.exp(-x))
     return temp
 
 
 def compute_error(m,x,y,rp):
     totalError = 0
     N = len(y)
-    x.shape=(N,123)
-    temp = 
-    for i in range(N):
-        temp[i]
+    x.shape=(N,124)
+    temp = rp*m.T.dot(m)
+    y_predict = sigmod(x.dot(m))
+    tt = -y*y_predict.T
+    temp+=float(1/N)*np.sum(np.log(1+np.exp(tt)))
+    #for i in range(N):
+        #temp+=float(1/N)*np.log(1+np.exp(tt[i,:]))
     #totalError=(y-x.todense().dot(m)-b)
     #totalError=totalError.T.dot(totalError)
     #totalError=np.sum(totalError)
 
-    return totalError/float(len(y))
+    return temp/float(len(y))
 
 
-def optimizer(x,y,x_test,y_test,starting_b,starting_m,learning_rate,num_iter):
-    b=starting_b
+def optimizer_NAG(x,y,x_test,y_test,starting_m,rp,num_iter):
+    #b=starting_b
     m=starting_m
-    error_list1 = np.arange(num_iter)
+    #error_list1 = np.arange(num_iter)
     error_list2 = np.arange(num_iter)
-    br, mr = compute_gradient(starting_b, starting_m, x_test, y_test, learning_rate)
+    #mr = compute_gradient_NAG(starting_m, x_test, y_test, rp)
 
     for i in range(num_iter):
-        if(i==0):
-            b,m=compute_gradient(b,m,x,y,learning_rate)
-            b+=br
-            m+=mr
-        else:
-            b, m = compute_gradient(b, m, x, y, learning_rate)
+        m = compute_gradient_NAG(m, x, y, rp)
         if(i%10==0):
-            temp = compute_error(b,m,x,y)
+            temp = compute_error(m,x,y,rp)
             print ('iter {0}:error={1}'.format(i,temp))
-        temp = compute_error(b, m, x, y)
-        error_list1[i] = temp
-        temp2 = compute_error(b,m,x_test,y_test)
+        #temp = compute_error(b, m, x, y)
+        #error_list1[i] = temp
+        temp2 = compute_error(m,x_test,y_test,rp)
         error_list2[i] = temp2
-    return[b,m,error_list1,error_list2]
+    return[m,error_list2]
 
 
-def compute_gradient(b_current,m_current,x,y,learning_rate):
+def compute_gradient_NAG(m_current,x,y,rp,size=200):
 
-    b_gradient = 0
+    #b_gradient = 0
     m_gradient = 0
 
     n=len(y)
     N = float(n)
     y.shape=(n,1)
-    m_current.shape=(123,1)
-    b_gradient = -(2/N)*(y-x.todense().dot(m_current)-b_current)
-    b_gradient = b_gradient.mean()
-    m_gradient = -(2/N)*x.todense().T*(((y-x.todense().dot(m_current)-b_current)))
-    m_gradient = m_gradient.mean(axis=0)
+    m_current.shape=(124,1)
+    #b_gradient = -(2/N)*(y-x.todense().dot(m_current)-b_current)
+    #b_gradient = b_gradient.mean()
+    #m_gradient = -(2/N)*x.todense().T*(((y-x.todense().dot(m_current)-b_current)))
+    #m_gradient = m_gradient.mean(axis=0
+    temp = random.randint(0, n - 200)
+    for i in range(size):
+        m_gradient+=(sigmod(x[temp+i,:].dot(m_current))-y[temp+i])*(x[temp+i,:].dot(m_current))
+    m_gradient/=N
 
-    new_b = b_current-(learning_rate*b_gradient)
-    new_m = m_current-(learning_rate*m_gradient)
-    return[new_b,new_m]
+    #new_b = b_current-(rp*b_gradient)
+    new_m = m_current-(rp*m_gradient)
+    return new_m
 
 
 def plot_data(error,error2):
@@ -78,22 +81,30 @@ def plot_data(error,error2):
 
 
 def Linear_regression():
-    data = load_svmlight_file('a9a.txt')
-    x_train,x_test,y_train,y_test = train_test_split(data[0],data[1],test_size=0.33,random_state=55)
+    x_train,y_train = load_svmlight_file('a9a.txt')
+    x_test,y_test = load_svmlight_file("a9a.t")
     y_t = len(y_train)
     y_e = len(y_test)
     y_train.reshape((y_t,1))
     y_test.reshape((y_e,1))
-    learning_rate = 0.01
-    init_b = 0.0
-    init_m = np.zeros(123)
-    num_iter = 100000
+    rp = 0.01
+    train = np.ones(y_t)
+    test = np.ones(y_e)
+    testt = np.zeros(y_e)
+    x_train=x_train.todense()
+    x_test=x_test.todense()
+    x_train = np.c_[x_train,train]
+    x_test = np.c_[x_test,testt]
+    x_test = np.c_[x_test,test]
+    #init_b = 0.0
+    init_m = np.zeros(124)
+    num_iter = 10
     error = 0
     error2 = 0
 
-    [b ,m,error,error2] = optimizer(x_train,y_train,x_test,y_test,init_b,init_m,learning_rate,num_iter)
+    [m,error,error2] = optimizer_NAG(x_train,y_train,x_test,y_test,init_m,rp,num_iter)
 
-    print ('final formula parmaters:\n b = {1}\n m={2}\n error of end = {3} \n'.format(num_iter,b,m,compute_error(b,m,x_train,y_train)))
+    print ('final formula parmaters:\n b = in m final number\n m={1}\n error of end = {2} \n'.format(num_iter,m,compute_error(b,m,x_train,y_train)))
 
     plot_data(error,error2)
 
